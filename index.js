9ebf04a24ce1a27d54280f1b87c49f35441e7012
ChahatBeniwal <137405867+ChahatBeniwal@users.noreply.github.com>
2023-12-14 13:33:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,51 +1,51 @@
 const cardArray = [
     {
         name:'carrot',
-        img:'./Images/carrot.png',
+        img:'./Images/carrot.PNG',
     },
     {
         name:'bunny',
-        img:'./Images/bunny.png',
+        img:'./Images/bunny.PNG',
     },
     {
         name:'chick',
-        img:'./Images/chick.png',
+        img:'./Images/chick.PNG',
     },
     {
         name:'rainbow',
-        img:'./Images/rainbow.png',
+        img:'./Images/rainbow.PNG',
     },
     {
         name:'egg',
-        img:'./Images/egg.png',
+        img:'./Images/egg.PNG',
     },
     {
         name:'egg2',
-        img:'./Images/egg2.png', 
+        img:'./Images/egg2.PNG', 
     },
     {
         name:'carrot',
-        img:'./Images/carrot.png',
+        img:'./Images/carrot.PNG',
     },
     {
         name:'bunny',
-        img:'./Images/bunny.png',
+        img:'./Images/bunny.PNG',
     },
     {
         name:'chick',
-        img:'./Images/chick.png',
+        img:'./Images/chick.PNG',
     },
     {
         name:'rainbow',
-        img:'./Images/rainbow.png',
+        img:'./Images/rainbow.PNG',
     },
     {
         name:'egg',
-        img:'./Images/egg.png',
+        img:'./Images/egg.PNG',
     },
     {
         name:'egg2',
-        img:'./Images/egg2.png', 
+        img:'./Images/egg2.PNG', 
     }
     
 ]
@@ -58,7 +58,7 @@ const cardWon=[]
 function createBoard(){
     for (let i =0;i<12;i++){
         const card=document.createElement('img')
-        card.setAttribute('src','./Images/cover.png')
+        card.setAttribute('src','./Images/cover.PNG')
         card.setAttribute('data-id',i )
         card.addEventListener('click',flipcard)
         gridDisplay.appendChild(card)
@@ -79,20 +79,20 @@ function checkformatch(){
     const FirstCardId = cardChosenId[0]
     const SecondCardId = cardChosenId[1]
     if (FirstCardId==SecondCardId){
-        cards[FirstCardId].setAttribute('src','./Images/cover.png')
-        cards[SecondCardId].setAttribute('src','./Images/cover.png')
+        cards[FirstCardId].setAttribute('src','./Images/cover.PNG')
+        cards[SecondCardId].setAttribute('src','./Images/cover.PNG')
         alert('Oops! You have clicked on the same image.')
     }
     if (cardChosen[0]==cardChosen[1]){
         alert("It's a Match!")
-        cards[FirstCardId].setAttribute('src','./Images/skin.png')
-        cards[SecondCardId].setAttribute('src','./Images/skin.png')
+        cards[FirstCardId].setAttribute('src','./Images/skin.PNG')
+        cards[SecondCardId].setAttribute('src','./Images/skin.PNG')
         cards[FirstCardId].removeEventListener('click',flipcard)
         cards[SecondCardId].removeEventListener('click',flipcard)
         cardWon.push(cardChosen)
     } else {
-        cards[FirstCardId].setAttribute('src','./Images/cover.png')
-        cards[SecondCardId].setAttribute('src','./Images/cover.png')
+        cards[FirstCardId].setAttribute('src','./Images/cover.PNG')
+        cards[SecondCardId].setAttribute('src','./Images/cover.PNG')
         alert('Oops! Try Again')
     }
     cardChosen=[]
